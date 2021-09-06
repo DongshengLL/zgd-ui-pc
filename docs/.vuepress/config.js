38a0d1638bb7e0a53549529484637de6bef42e74
handsomeMan <598595404@qq.com>
2021-09-06 14:37:08 +0800

    修改项目名称

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,53 @@
+/*
+ * @Author: 刘东升
+ * @Date: 2021-08-10 17:54:12
+ * @LastEditors: 刘东升
+ * @LastEditTime: 2021-09-06 14:23:49
+ * @Descripttion: 
+ * @version: 
+ */
 const path = require('path')
 module.exports = {
-    base:'/sakura-ui/',
-    title: 'Sakura UI',
-    description: 'Inspiration from heian sakura',
+    base: '/zgd-ui-pc/',
+    title: 'zgd-ui-pc',
+    description: 'zgd-ui-pc',
     head: [
-      ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
     ],
     themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Github', link: 'https://github.com/Firenzia/sakura-ui/' },
-      ],
-      sidebar: [
-          {
-              title: '开发指南',
-              collapsable: true,
-              children: [
-                'views/guide/install.md',
-                'views/guide/get-started.md'
-              ]
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'https://github.com/DongshengLL/zgd-ui-pc/' },
+        ],
+        sidebar: [{
+                title: '开发指南',
+                collapsable: true,
+                children: [
+                    'views/guide/install.md',
+                    'views/guide/get-started.md'
+                ]
             },
             {
-              title: '设计',
-              collapsable: true,
-              children: [
-                'views/design/color/',
-              ]
+                title: '设计',
+                collapsable: true,
+                children: [
+                    'views/design/color/',
+                ]
             },
             {
-              title: '组件',
-              collapsable: true,
-              children: [
-                'views/components/basic/',
-                'views/components/form/',
-                'views/components/navigation/',
-                'views/components/notice/',
-                'views/components/other/'
-              ]
+                title: '组件',
+                collapsable: true,
+                children: [
+                    'views/components/basic/',
+                    'views/components/form/',
+                    'views/components/navigation/',
+                    'views/components/notice/',
+                    'views/components/other/'
+                ]
             },
         ]
-      },
-    scss:{ //配置 scss 根目录
-      includePaths: [path.join(__dirname, '../../style')]
+    },
+    scss: { //配置 scss 根目录
+        includePaths: [path.join(__dirname, '../../style')]
     }
-  }
+}
